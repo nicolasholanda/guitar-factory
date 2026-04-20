e59c9d74de82b63f0e517c85c452ee439a943578
nicolasholanda <nikolas.nhm14@gmail.com>
2026-04-20 09:01:49 -0300

    Initial commit

---
+++ src/main/java/com/guitarfactory/GuitarFactoryApplication.java
@@ -0,0 +1,12 @@
+package com.guitarfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuitarFactoryApplication.class, args);
+    }
+}
